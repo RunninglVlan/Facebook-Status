aba2e1271b551d5073cc0403421a6491fa845cd5
RunninglVlan <RedCapShortCut@gmail.com>
2016-07-29 00:56:56 +0300

    Refactoring: - removed Constants * moved constants to scripts that use them * renamed time constants to reflect their purpose * made Statuses.currentPath private + added helper methods for checking which status page to open * changed logic of checking for login page

--- background.js
+++ background.js
@@ -1,21 +1,22 @@
-﻿// import Constants.js, Statuses.js
+﻿// import Statuses.js
 
 var statuses = new Statuses();
 
 window.addEventListener("load", () => {
+	var UPDATE_TIME_MS = 1000 * 30 - 1;
 	statuses.fetch();
-	window.setInterval(() => statuses.fetch(), Constants.TIME_30S);
+	window.setInterval(() => statuses.fetch(), UPDATE_TIME_MS);
 }, false);
 
 chrome.browserAction.onClicked.addListener(() => {
-	chrome.tabs.query({ url: Constants.DESKTOP_URL + '*' }, tabs => {
+	chrome.tabs.query({ url: statuses.DESKTOP_URL + '*' }, tabs => {
 		if (tabs.length === 0) {
-			var urlToOpen = Constants.DESKTOP_URL;
-			if (statuses.currentPath === Constants.REQUESTS_PATH) {
+			var urlToOpen = statuses.DESKTOP_URL;
+			if (statuses.isRequestsIconShown()) {
 				urlToOpen = "https://www.facebook.com/friends/requests/";
-			} else if (statuses.currentPath === Constants.MESSAGES_PATH) {
+			} else if (statuses.isMessagesIconShown()) {
 				urlToOpen = "https://www.facebook.com/messages/";
-			} else if (statuses.currentPath === Constants.NOTIFICATIONS_PATH) {
+			} else if (statuses.isNotificationsIconShown()) {
 				urlToOpen = "https://www.facebook.com/notifications/";
 			}
 			chrome.tabs.create({ url: urlToOpen });
--- js/Statuses.js
+++ js/Statuses.js
@@ -1,31 +1,36 @@
-﻿// import Constants.js
+﻿function Statuses() {
+	var EXTENSION_NAME = "Facebook Status";
+	var STATE_DONE = 4, STATUS_OK  = 200;
+	var ICONS_UPDATE_TIME_MS = 5000;
+	var MOBILE_URL   = "https://m.facebook.com/";
+	this.DESKTOP_URL = "https://www.facebook.com/";
+	var DEFAULT_PATH = "img/default.png", ERROR_PATH = "img/error.png";
+	var REQUESTS_PATH      = "img/requests.png";
+	var MESSAGES_PATH      = "img/messages.png";
+	var NOTIFICATIONS_PATH = "img/notifications.png";
 
-function Statuses() {
-	var DEFAULT_PATH = "img/default.png";
-	var ERROR_PATH =   "img/error.png";
-	var requestsCount = 0;
-	var messagesCount = 0;
-	var notificationsCount = 0;
+	var requestsCount = 0, messagesCount = 0, notificationsCount = 0;
 	var xhr = new XMLHttpRequest();
-	xhr.timeout = Constants.TIME_1S * 5;
+	xhr.timeout = 5000;
 	xhr.responseType = "document";
 	var intervalID;
 	var updateIconArray = [];
-	this.currentPath = DEFAULT_PATH;
+	var currentPath = DEFAULT_PATH;
 
 	/**
 	 * First tries to select statuses from Mobile site as it's most lightweight.
 	 * If it'll fail, tries to select statuses from Desktop site.
 	 * If it'll fail too, user might be not logged in, this is checked too.
 	 * If user is logged in, there's an unexpected error.
+	 * XMLHttpRequest is used instead of Fetch API as latter doesn't return necessary data in response.
 	 */
 	this.fetch = function (url, callback) {
 		requestsCount = messagesCount = notificationsCount = 0;
 		if (arguments.length === 0) {
-			this.fetch(Constants.MOBILE_URL, parseMobile);
+			this.fetch(MOBILE_URL, parseMobile);
 		} else {
 			xhr.onload = () => {
-				if (xhr.readyState === Constants.STATE_DONE && xhr.status === Constants.STATUS_OK) {
+				if (xhr.readyState === STATE_DONE && xhr.status === STATUS_OK) {
 					try {
 						callback(xhr.response);
 						window.clearInterval(intervalID);
@@ -33,7 +38,7 @@ function Statuses() {
 						updateIcon();
 					} catch (e) {
 						if (!e.desktop) {
-							this.fetch(Constants.DESKTOP_URL, parseDesktop);
+							this.fetch(this.DESKTOP_URL, parseDesktop);
 						}
 					}
 				}
@@ -42,6 +47,7 @@ function Statuses() {
 			xhr.send(null);
 		}
 	};
+
 	var parseMobile = response => {
 		var COUNT_CLASS = "._59tg";
 		requestsCount = response.querySelector("#requests_jewel").querySelector(COUNT_CLASS).innerText;
@@ -56,10 +62,10 @@ function Statuses() {
 		} catch (e) {
 			chrome.browserAction.setIcon({ path: ERROR_PATH });
 			chrome.browserAction.setBadgeText({ text: '!' });
-			if (response.getElementsByName("email").length === 0) {
-				chrome.browserAction.setTitle({ title: Constants.EXTENSION_NAME + ": Unexpected error" });
+			if (response.querySelector("#login_form")) {
+				chrome.browserAction.setTitle({ title: EXTENSION_NAME + ": Login to Facebook first" });
 			} else {
-				chrome.browserAction.setTitle({ title: Constants.EXTENSION_NAME + ": Login to Facebook first" });
+				chrome.browserAction.setTitle({ title: EXTENSION_NAME + ": Unexpected error" });
 			}
 			// Rethrowing so that updateIcon wouldn't be called
 			e.desktop = true;
@@ -68,7 +74,7 @@ function Statuses() {
 	};
 	var updateIcon = function (imagePath, count) {
 		if (arguments.length === 0) {
-			chrome.browserAction.setTitle({ title: Constants.EXTENSION_NAME });
+			chrome.browserAction.setTitle({ title: EXTENSION_NAME });
 			if (newRequestsExist()) {
 				updateIconArray.push(updateRequestsIcon);
 			}
@@ -84,29 +90,33 @@ function Statuses() {
 				updateIconArray[0]();
 			} else {
 				updateIcons();
-				intervalID = window.setInterval(() => updateIcons(), Constants.TIME_1S * 5);
+				intervalID = window.setInterval(() => updateIcons(), ICONS_UPDATE_TIME_MS);
 			}
 		} else {
 			chrome.browserAction.setIcon({ path: imagePath });
 			chrome.browserAction.setBadgeText({ text: count.toString() });
-			this.currentPath = imagePath;
+			currentPath = imagePath;
 		}
 	}.bind(this);
 	var updateRequestsIcon = () => {
-		updateIcon(Constants.REQUESTS_PATH, requestsCount);
+		updateIcon(REQUESTS_PATH, requestsCount);
 	};
 	var updateMessagesIcon = () => {
-		updateIcon(Constants.MESSAGES_PATH, messagesCount);
+		updateIcon(MESSAGES_PATH, messagesCount);
 	};
 	var updateNotificationsIcon = () => {
-		updateIcon(Constants.NOTIFICATIONS_PATH, notificationsCount);
+		updateIcon(NOTIFICATIONS_PATH, notificationsCount);
 	};
+	var newRequestsExist      = () => requestsCount > 0;
+	var newMessagesExist      = () => messagesCount > 0;
+	var newNotificationsExist = () => notificationsCount > 0;
 	var updateIcons = () => {
 		var nextFunction = updateIconArray.shift();
 		nextFunction();
 		updateIconArray.push(nextFunction);
 	};
-	var newRequestsExist      = () => requestsCount > 0;
-	var newMessagesExist      = () => messagesCount > 0;
-	var newNotificationsExist = () => notificationsCount > 0;
+
+	this.isRequestsIconShown       = () => currentPath === REQUESTS_PATH;
+	this.isMessagesIconShown       = () => currentPath === MESSAGES_PATH;
+	this.isNotificationsIconShown  = () => currentPath === NOTIFICATIONS_PATH;
 }
